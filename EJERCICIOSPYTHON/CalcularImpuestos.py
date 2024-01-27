19bd3aa3f95b7a7c29a3145c7225a5188528912c
KevinGalvisSA <kesangari201006@gmail.com>
2024-01-26 19:57:44 -0500

    Finalizacion de la calculadora de impuestos

--- EJERCICIOSPYTHON/CalcularImpuestos.py
+++ EJERCICIOSPYTHON/CalcularImpuestos.py
@@ -13,27 +13,27 @@ Impuesto6 = 0.40
 
 if ingresos < 100:
     Impuesto = ingresos * Impuesto1
-    ImpuestoTotal = ingresos + Impuesto
+    print ("El impuesto a pagar es de: ",Impuesto)
     print ("Usted se encuentra en la categoria: \Ingresos bajos/")
 elif ingresos > 100 and ingresos <= 200:
     Impuesto = ingresos * Impuesto2
-    ImpuestoTotal = ingresos + Impuesto
+    print ("El impuesto a pagar es de: ",Impuesto)
     print ("Usted se encuentra en la categoria: -Ingresos medio bajos-")
 elif ingresos > 200 and ingresos <= 300:
     Impuesto = ingresos * Impuesto3
-    ImpuestoTotal = ingresos + Impuesto
+    print ("El impuesto a pagar es de: ",Impuesto)
     print ("Usted se encuentra en la categoria: --Ingresos medio--")
 elif ingresos > 300 and ingresos <= 400:
     Impuesto = ingresos * Impuesto4
-    ImpuestoTotal = ingresos + Impuesto
+    print ("El impuesto a pagar es de: ",Impuesto)
     print ("Usted se encuentra en la categoria: ¨Ingresos medio altos¨")
 elif ingresos > 400 and ingresos <=500:
-    print ("Usted se encuentra en la categoria: ¨¨Ingresos altos¨¨")
     Impuesto = ingresos * Impuesto5
-    ImpuestoTotal = ingresos + Impuesto
+    print ("El impuesto a pagar es de: ",Impuesto)
+    print ("Usted se encuentra en la categoria: ¨¨Ingresos altos¨¨")
 elif ingresos > 500:
     Impuesto = ingresos * Impuesto6
-    ImpuestoTotal = ingresos + Impuesto
+    print ("El impuesto a pagar es de: ",Impuesto)
     print ("Usted se encuentra en la categoria: **Ingresos super altos**")
 
 
