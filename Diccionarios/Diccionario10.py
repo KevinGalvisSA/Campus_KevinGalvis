10393ddde6d15255c45338fd26b6edb972637143
KevinGalvisSA <kesangari201006@gmail.com>
2024-02-01 21:50:25 -0500

    Nuevos ejercicios en diccionarios

---
+++ Diccionarios/Diccionario10.py
@@ -0,0 +1,47 @@
+
+DiccionarioContactos = {
+    'contacto1': {
+        'nombre': 'Rafael Galvis',
+        'telefono': '123-456-7890',
+        'Correo': 'Ragaes1997@gmail.com'
+    },
+    'contacto2': {
+        'nombre': 'Lesly Arias',
+        'telefono': '149-260-5597',
+        'Correo': 'Less2029@gmail.com'
+    },
+    'contacto3': {
+        'nombre': 'Kevin Galvis',
+        'telefono': '020-010-2006',
+        'Correo': 'kesangari201006@gmail.com'
+    }
+}
+
+
+nombreContacto1 = DiccionarioContactos['contacto1']['nombre']
+nombreContacto2 = DiccionarioContactos['contacto2']['nombre']
+nombreContacto3 = DiccionarioContactos['contacto3']['nombre']
+
+telefonoContacto1 = DiccionarioContactos['contacto1']['telefono']
+telefonoContacto2 = DiccionarioContactos['contacto2']['telefono']
+telefonoContacto3 = DiccionarioContactos['contacto3']['telefono']
+
+CorreoContacto1 = DiccionarioContactos['contacto1']['Correo']
+CorreoContacto2 = DiccionarioContactos['contacto2']['Correo']
+CorreoContacto3 = DiccionarioContactos['contacto3']['Correo']
+
+
+print("Nombre de contacto1:", nombreContacto1)
+print("Nombre de contacto2:", nombreContacto2)
+print("Nombre de contacto3:", nombreContacto3)
+print("Teléfono de contacto1:", telefonoContacto1)
+print("Teléfono de contacto2:", telefonoContacto2)
+print("Teléfono de contacto3:", telefonoContacto3)
+print("Correo de contacto1:", CorreoContacto1)
+print("Correo de contacto2:", CorreoContacto2)
+print("Correo de contacto3:", CorreoContacto3)
+
+
+
+
+
--- Diccionarios/Diccionario9.py
+++ Diccionarios/Diccionario9.py
@@ -1,1 +1,10 @@
-#En desarrollo
+
+Diccionario = {
+    'Numero1': 1, 
+    'Numero2': 2, 
+    'Numero3': 3, 
+    'Numero4': 4
+}
+
+for Clave, Valor in Diccionario.items():
+    print(f"Clave: {Clave}, Valor: {Valor}")
